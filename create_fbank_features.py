f86341c6800a3601d773f67b66814b1228bd76d8
Elijah Gutierrez <elijahjgutierrez99@gmail.com>
2023-11-22 01:28:25 +0000

    testing q2

---
+++ create_fbank_features.py
@@ -0,0 +1,62 @@
+import torchaudio
+import json
+import pathlib
+import os
+import torch
+from torchaudio.compliance import kaldi
+
+def extract_fbank_from_file(fname):
+	fname = pathlib.Path(fname)
+	json_to_write = {}	
+
+	# open the file and load the JSON data
+	with open(fname, 'r') as f:
+		fbank_data = json.load(f)
+
+	for key in fbank_data:
+		key_split = key.split('_')
+		
+		spk_id = key_split[0]
+		fbank_id = key_split[1]
+
+		fbank_obj = fbank_data[key]
+		wav_path = fbank_obj['wav']
+		
+		# load wav file 
+		loaded_wav, sample_rate = torchaudio.load(wav_path)
+		fbank_features = kaldi.fbank(loaded_wav)	
+		
+		# save fbank features to TIMIT/data/fbanks/fname/spk_id/fbank_id	
+		
+		path_to_save = os.path.join(f'/rds/user/ejg84/hpc-work/MLMI2/TIMIT/data/fbanks/{fname.stem}', spk_id) 
+		
+		if os.path.exists(path_to_save) != True:
+			os.mkdir(path_to_save)			
+
+		torch.save(fbank_features, os.path.join(path_to_save,fbank_id))
+
+		# construct json to write
+		val = {}	
+		val['duration'] = fbank_obj['duration']
+		val['phn'] = fbank_obj['phn'] 	
+		val['spk_id'] = spk_id
+		val['fbank'] = path_to_save
+
+		json_to_write[key] = val
+
+	path_to_write = f'{fname.stem}_fbank.json'
+	json_str = json.dumps(json_to_write)
+	
+	g = open(path_to_write, "w+")
+	g.write(json_str)
+	g.close	
+			
+files_to_extract = ['train.json', 'test.json', 'dev.json']
+
+for f in files_to_extract:
+	print(f'processing file {f}')
+	extract_fbank_from_file(f)
+	 
+	
+print('done')
+
---
+++ phone_set_out_vocab.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import json
+
+
+def get_vocab(fname):
+	vocab = set(['_'])
+	with open(fname, 'r') as f:
+		while line := f.readline():
+			if len(line.split(':')[1].strip('\n ')) > 0:
+				vocab.add(line.split(':')[1].strip('\n '))
+	return vocab
+
+
+
+def write_vocab(fname):
+	vocab = get_vocab(fname)
+	with open('vocab_39.txt', 'w+') as f:
+		for v in vocab:
+			f.write(v+'\n')	
+
+	f.close()
+
+# write_vocab('phone_map')
+
+
+# visualise phone freqs
+def visualise_phone_frequencies():
+	train_file = 'train.json'
+	N = 0
+	phn_count_dict = {}	
+	
+	with open('vocab_39.txt') as f:
+		while line := f.readline():
+			phn = line.strip()
+			phn_count_dict[phn] = 0
+        
+	with open('train.json', 'r') as g:
+                fbank_data = json.load(g)
+
+	for key in fbank_data:
+		fbank_obj = fbank_data[key]
+		phones = fbank_obj['phn']
+		for phn in phones.split():
+			phn_count_dict[phn] += 1
+			N += 1
+		
+	f.close()
+	g.close()
+
+	plt.barh(*zip(*phn_count_dict.items()))
+	plt.show()
+	
+visualise_phone_frequencies()
