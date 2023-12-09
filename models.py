5923faaf7c71b30718b137e23180432c7d7135f7
Elijah Gutierrez <elijahjgutierrez99@gmail.com>
2023-12-09 10:47:22 +0000

    got proper results for uni

--- models.py
+++ models.py
@@ -4,10 +4,10 @@ class BiLSTM(nn.Module):
     def __init__(self, num_layers, in_dims, hidden_dims, out_dims, dropout, is_bidir):
         super().__init__()
         self.num_layers = num_layers
-        self.lstm = nn.LSTM(in_dims, hidden_dims, self.num_layers, bidirectional=is_bidir)
+        self.lstm = nn.LSTM(in_dims, hidden_dims, self.num_layers, bidirectional=False)
         if num_layers > 1:
-            self.lstm = nn.LSTM(in_dims, hidden_dims, self.num_layers, bidirectional=is_bidir, dropout=dropout)
-        self.proj = nn.Linear(hidden_dims * 2, out_dims)
+            self.lstm = nn.LSTM(in_dims, hidden_dims, self.num_layers, bidirectional=False, dropout=dropout)
+        self.proj = nn.Linear(hidden_dims * 1, out_dims)
         self.dropout_layer = nn.Dropout(dropout)
 
     def forward(self, feat):
